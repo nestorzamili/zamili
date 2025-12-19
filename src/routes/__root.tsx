d39c26cf8cd38b9a79a37a75e816d34f68817325
Nestor <nestorzamili@gmail.com>
2025-12-19 17:20:38 +0700

    feat: update icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -52,12 +52,25 @@ export const Route = createRootRoute({
 			},
 			{
 				rel: 'icon',
+				type: 'image/x-icon',
+				href: '/favicon.ico',
+			},
+			{
+				rel: 'icon',
 				type: 'image/png',
-				href: '/icon.png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png',
 			},
 			{
 				rel: 'apple-touch-icon',
-				href: '/icon.png',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png',
 			},
 			{
 				rel: 'manifest',
